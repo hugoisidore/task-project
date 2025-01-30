33d2825b2f98f1e17a2b9d5012794e7e4bf67bf3
hugoisidore <hugo.isidore@oclock.school>
2025-01-30 12:10:07 +0100

    feat: adding of categories for tasks

---
+++ src/main/java/com/example/taskmanager/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.taskmanager.repository;
+
+import com.example.taskmanager.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
+}
